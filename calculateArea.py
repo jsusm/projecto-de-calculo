5efd00495cde9e55ebeed8a0e3d9e41c36cb9556
Jesus Marcano <jesusjaviermarcanomora.2005@gmail.com>
2026-03-06 11:29:10 -0400

    pasar metodo del informe a codigo

--- calculateArea.py
+++ calculateArea.py
@@ -1,8 +1,33 @@
+import math
 import numpy as np
 import scipy.integrate as integrate
 
 #Recibir parametros de la interfaz
-def calculate_area(f_points, g_points, x1, x2):
-    print("Calculando area con puntos f:", f_points)
-    print("Calculando area con puntos g:", g_points)
-    print("Subintervalos en x:", x1, x2)
+def calculate_area(interpolators: list[tuple[float, float]], a: int, b: int):
+    # Numero de interpolantes
+    n = len(interpolators)
+
+    # Debemos integrar la base de los polinomios en este caso la base canonica
+    # para construir el b del sistema de ecuaciones Aw = k
+    k = np.empty(n)
+    for i in range(n):
+        # calculamos la integral desde 'a' hasta 'b' de x^i
+        k[i] = (1/(i+1)) * (math.pow(b, i + 1) - math.pow(a, i + 1))
+
+    # Contruimos la matriz de Vandermore
+    A = np.empty((n, n))
+    for i in range(n):
+        for j in range(n):
+            # A_{i,j} = x_j^i
+            A[i][j] = math.pow(interpolators[j][0], i)
+
+    # Calculamos los w_i
+    w = np.linalg.solve(A, k)
+
+    area: float = 0
+    for i in range(n):
+        area += w[i] * interpolators[i][1]  # pyright: ignore[reportAny]
+
+    return area
+
+print(calculate_area([(0, 0), (1/3, 1/27), (1, 1)], 0, 1))
